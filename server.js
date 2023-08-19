f34109feb102f00329309eb569f926723f5bbf9a
shrabanti1 <jharnamazumdar78623@gmail.com>
2023-08-20 02:14:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/api/v1/product", productRoutes);
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 //rest api
-app.use('*',functon(req,res){
+app.use('*',function(req,res){
   res.sendFile((path.join(__dirname, "./client/build/index.html")));
 });
 
